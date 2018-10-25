a5f989a8ae3d36a0c4803cb55c72c2fbe603c42e
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-10-25 18:57:54 +0800

    fix truffle setting

--- truffle.js
+++ truffle.js
@@ -1,18 +1,18 @@
+const path = require('path');
 const config = require('./kaizen.json');
 var HDWalletProvider = require('truffle-hdwallet-provider');
-var mnemonic = 'concert load couple harbor equip island argue ramp clarify fence smart topic';
 
 module.exports = {
-  contracts_build_directory: config.ethereum.build_output_path,
+  contracts_build_directory: path.resolve(config.ethereum.build_output_path),
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*'
     },
-    ropsten: {
-      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/<Your Infura API Key>'),
-      network_id: 3 // official id of the ropsten network
+    deployment: {
+      provider: new HDWalletProvider(config.ethereum.mnemonic, config.ethereum.provider),
+      network_id: config.ethereum.network_id
     }
   }
 };
